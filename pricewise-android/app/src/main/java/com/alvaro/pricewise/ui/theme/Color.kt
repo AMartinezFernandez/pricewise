bb0827f6d3b37ec0952c4a97bdb8a83f8099d5b2
Álvaro Martínez Fernández <amarfer26@gmail.com>
2026-02-23 19:11:50 +0100

    chore: añadir paleta de colores PriceWise como recursos

---
+++ pricewise-android/app/src/main/java/com/alvaro/pricewise/ui/theme/Color.kt
@@ -0,0 +1,19 @@
+package com.alvaro.pricewise.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Paleta PriceWise
+
+// Oscuros (fondos, barras, superficies)
+val PwDarkNavy = Color(0xFF1E2A33)
+val PwSlateGray = Color(0xFF2C3B46)
+
+// Naranjas (acento principal, botones, highlights)
+val PwOrange = Color(0xFFF39A2E)
+val PwOrangeDark = Color(0xFFD87E1F)
+val PwOrangeLight = Color(0xFFFFB14A)
+
+// Azules (informacion, enlaces, secundario)
+val PwCyan = Color(0xFF6FD3F7)
+val PwCyanMedium = Color(0xFF4BB8E6)
+val PwCyanDark = Color(0xFF2C8DB5)
